64715f5a1f96c1b858a9ebaffc1022848dd55960
Dimon98165 <dimonkuznecov2007@gmail.com>
2026-07-01 19:40:59 +0300

    первый бэкенд на C++

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,23 @@
 #include <iostream>
-
+#include "headers/crow.h"
+#include <windows.h>
 int main(int, char**){
-    std::cout << "Hello, from LiteFox!\n";
+    crow::App<crow::UTF8> app;
+    CROW_ROUTE(app, "/")
+    ([]() {
+        crow::response res;
+        res.set_header("Content-Type", "text/html; charset=utf-8");
+        
+        // 2. Wrap the text in a native std::string container to protect the emoji bytes
+        res.body = std::string("Hello from LiteFox! LiteFox Engine is running successfully! 🚀🦊");
+        
+        return res;
+    });
+
+    SetConsoleOutputCP(CP_UTF8);
+    SetConsoleCP(CP_UTF8);
+    std::cout << "Привет мир!" << std::endl;
+
+    // Запуск сервера
+    app.port(8080).multithreaded().run();
 }
